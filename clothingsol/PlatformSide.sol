df93e66fa6222210744d5b3aee96c7bce18f04f1
leofashion <leofashion@sina.com>
2019-04-09 11:49:48 +0800

    add computer

--- clothingsol/PlatformSide.sol
+++ clothingsol/PlatformSide.sol
@@ -1,7 +1,37 @@
 pragma solidity ^0.4.22;
 
+library SafeMath {
+      function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+          return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+      }
+
+      function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+      }
+
+      function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+      }
+
+      function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+      }
+    }
+
 contract PlatformSide{
 
+  using SafeMath for uint256;
   address platformAddr;
 
 
@@ -38,7 +68,7 @@ contract PlatformSide{
   //Set up a gas
   function setGas(string oriderID,uint256 _gasvalue) public {
     if(isMaster(msg.sender)){
-      orderGas[oriderID] = _gasvalue;
+      orderGas[oriderID] = _gasvalue.mul(0.1);
     }
   }
   //Get gas
